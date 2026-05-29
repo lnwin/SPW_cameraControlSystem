af40434050a3b88e94fb1d6352537cc86093f6a6
lnwin <405824853@qq.com>
2026-05-29 21:33:13 +0800

    添加了，右下角鼠标拖动窗体

--- hudwindow.cpp
+++ hudwindow.cpp
@@ -4,11 +4,14 @@
 #include <QQuickItem>
 #include <QDebug>
 #include <QResizeEvent>
+#include <QMouseEvent>
+#include <QCursor>
 
 HudWindow::HudWindow(UiController* ctrl, const QString& greenLogoPath, QWidget* parent)
     : QQuickWidget(parent)
 {
     setWindowFlags(Qt::FramelessWindowHint | Qt::Window);
+    setMouseTracking(true);
     rootContext()->setContextProperty("uiCtrl", ctrl);
     rootContext()->setContextProperty("greenLogoUrl",
         greenLogoPath.isEmpty() ? QString() : QUrl::fromLocalFile(greenLogoPath).toString());
@@ -34,6 +37,48 @@ void HudWindow::resizeEvent(QResizeEvent* e)
 {
     QQuickWidget::resizeEvent(e);
     syncVideoGeometry();
+}
+
+bool HudWindow::inResizeZone(const QPoint& pos) const
+{
+    return pos.x() >= width() - kEdge && pos.y() >= height() - kEdge;
+}
+
+void HudWindow::mousePressEvent(QMouseEvent* e)
+{
+    if (e->button() == Qt::LeftButton && inResizeZone(e->pos())) {
+        resizing_    = true;
+        resizeStart_ = e->globalPos();
+        sizeAtStart_ = size();
+        e->accept();
+        return;
+    }
+    QQuickWidget::mousePressEvent(e);
+}
+
+void HudWindow::mouseMoveEvent(QMouseEvent* e)
+{
+    if (resizing_) {
+        QPoint delta = e->globalPos() - resizeStart_;
+        int w = qMax(640, sizeAtStart_.width()  + delta.x());
+        int h = qMax(400, sizeAtStart_.height() + delta.y());
+        resize(w, h);
+        e->accept();
+        return;
+    }
+    // 更新鼠标指针
+    setCursor(inResizeZone(e->pos()) ? Qt::SizeFDiagCursor : Qt::ArrowCursor);
+    QQuickWidget::mouseMoveEvent(e);
+}
+
+void HudWindow::mouseReleaseEvent(QMouseEvent* e)
+{
+    if (resizing_) {
+        resizing_ = false;
+        e->accept();
+        return;
+    }
+    QQuickWidget::mouseReleaseEvent(e);
 }
 
 void HudWindow::syncVideoGeometry()
--- hudwindow.h
+++ hudwindow.h
@@ -2,9 +2,8 @@
 
 #include <QQuickWidget>
 #include <QWidget>
+#include <QPoint>
 #include "uicontroller.h"
-
-class ZoomPanImageView;
 
 class HudWindow : public QQuickWidget
 {
@@ -15,8 +14,18 @@ public:
 
 protected:
     void resizeEvent(QResizeEvent* e) override;
+    void mousePressEvent(QMouseEvent* e) override;
+    void mouseMoveEvent(QMouseEvent* e) override;
+    void mouseReleaseEvent(QMouseEvent* e) override;
+    void mouseMoveEvent_cursor(QMouseEvent* e);
 
 private:
     void syncVideoGeometry();
+    bool inResizeZone(const QPoint& pos) const;
+
     QWidget* videoContainer_ = nullptr;
+    bool     resizing_       = false;
+    QPoint   resizeStart_;
+    QSize    sizeAtStart_;
+    static constexpr int kEdge = 12; // 右下角热区大小
 };
